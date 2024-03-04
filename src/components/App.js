5086534981d27a0a06383d949ae99b43db56d36a
Dawood Alkawaz <dalkawaz@gmail.com>
2024-03-04 12:46:53 +0300

    Solution

--- src/components/App.js
+++ src/components/App.js
@@ -16,15 +16,25 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
-
+{
+  /* write an <About> component here */
+}
+function About() {
+  return (
+    <div id="about">
+      <h2>About.</h2>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
       <NavBar />
       {/* add the <Home> component here */}
+      <Home />
       {/* add your <About> component here */}
+      <About />
     </div>
   );
 }
